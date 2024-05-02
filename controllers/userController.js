9fca472400dda8f7a6b75770379418b67bbd1273
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-02 15:14:35 -0600

    Add creating users if db is empty

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,11 @@
 const User = require('../models/userModel');
+const usersData = require('../data/users');
 
 //get all users
 module.exports.getUsers = async (req, res) => {
   const users = await User.find();
+  if (users.length === 0) {
+    await User.insertMany(usersData);
+  }
   res.render('users', { title: 'users', users });
 };
