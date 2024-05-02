8181f91eaf91e746a9a6beb79fb6ca610079337f
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-02 12:47:59 -0600

    Fix typo with mongoose

--- db/conn.js
+++ db/conn.js
@@ -1,9 +1,9 @@
-const moongose = require('mongoose');
+const mongoose = require('mongoose');
 
 const connectionString = process.env.ATLAS_URI || '';
 const mongoDbConfig = async () => {
   try {
-    const result = await moongose.connect(connectionString);
+    const result = await mongoose.connect(connectionString);
     console.log('Conneted to database succesffuly');
   } catch (err) {
     console.log('Failed to connect to database: ', err);
