6adb17521ebee4dbee9836dbd93a0daf0af90c33
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-02 20:02:23 -0600

    Add post controller

---
+++ controllers/postController.js
@@ -0,0 +1,87 @@
+const mongoose = require('mongoose');
+const User = require('../models/userModel');
+const Post = require('../models/postModel');
+
+//GET posts
+module.exports.getPosts = async (req, res, next) => {
+  //for userId query
+  if (req.query.userId) {
+    if (!mongoose.Types.ObjectId.isValid(req.query.userId)) {
+      next();
+      return;
+    }
+    let userPosts = await Post.find({ userId: req.query.userId });
+    if (userPosts.length > 0) {
+      res.render('posts', { title: 'posts', posts: userPosts });
+    } else {
+      next();
+      return;
+    }
+  } else {
+    const posts = await Post.find();
+    res.render('posts', { title: 'posts', posts });
+  }
+
+  //res.json(posts);
+};
+
+//Create post
+module.exports.createPost = async (req, res) => {
+  const userExists = await User.findById(req.body.userId);
+  if (!userExists) {
+    return res.json({ error: 'Not user found' });
+  }
+  try {
+    const post = await Post.create(req.body);
+    res.json(post);
+  } catch (err) {
+    return res.json({ error: 'error: ' + err.message });
+  }
+};
+
+//GET post by id
+module.exports.getPostById = async (req, res, next) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.json({ error: 'Id is not valid' }).status(404);
+  }
+  const post = await Post.findById(req.params.id);
+  if (post) {
+    res.json(post);
+  } else {
+    next();
+  }
+};
+
+//Update post
+module.exports.updatePostById = async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.json({ error: 'Id is not valid' }).status(404);
+  }
+  try {
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!post) {
+      return res.status(404).json({ error: 'Not such post' });
+    }
+    res.json(post);
+  } catch (err) {
+    res.json({ error: `Error ${err.message}` });
+  }
+};
+
+//Delete post
+module.exports.deletePostById = async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.json({ error: 'Id is not valid' }).status(404);
+  }
+  try {
+    const post = await Post.findByIdAndDelete(req.params.id);
+    if (!post) {
+      return res.json({ error: 'Post not found' });
+    }
+    res.json(post);
+  } catch (err) {
+    res.json({ error: `Error ${err.message}` });
+  }
+};
--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -3,88 +3,16 @@ const mongoose = require('mongoose');
 const router = express.Router();
 const Post = require('../models/postModel');
 const User = require('../models/userModel');
-
+const postController = require('../controllers/postController');
 
 //GET and POST posts
-router
-  .route('/')
-  .get(async (req, res, next) => {
-    //for userId query
-    if (req.query.userId) {
-      if (!mongoose.Types.ObjectId.isValid(req.query.userId)) {
-        next();
-        return;
-      }
-      let userPosts = await Post.find({ userId: req.query.userId });
-      if (userPosts.length > 0) {
-        res.render('posts', { title: 'posts', posts: userPosts });
-      } else {
-        next();
-        return;
-      }
-    } else {
-      const posts = await Post.find();
-      res.render('posts', { title: 'posts', posts });
-    }
-
-    //res.json(posts);
-  })
-  .post(async (req, res) => {
-    const userExists = await User.findById(req.body.userId);
-    if (!userExists) {
-      return res.json({ error: 'Not user found' });
-    }
-    try {
-      const post = await Post.create(req.body);
-      res.json(post);
-    } catch (err) {
-      return res.json({ error: 'error: ' + err.message });
-    }
-  });
+router.route('/').get(postController.getPosts).post(postController.createPost);
 
 //GET posts by id, PUT by id, DELETE by id
 router
   .route('/:id')
-  .get(async (req, res, next) => {
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-      return res.json({ error: 'Id is not valid' }).status(404);
-    }
-    const post = await Post.findById(req.params.id);
-    if (post) {
-      res.json(post);
-    } else {
-      next();
-    }
-  })
-  .put(async (req, res) => {
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-      return res.json({ error: 'Id is not valid' }).status(404);
-    }
-    try {
-      const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-      });
-      if (!post) {
-        return res.status(404).json({ error: 'Not such post' });
-      }
-      res.json(post);
-    } catch (err) {
-      res.json({ error: `Error ${err.message}` });
-    }
-  })
-  .delete(async (req, res) => {
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-      return res.json({ error: 'Id is not valid' }).status(404);
-    }
-    try {
-      const post = await Post.findByIdAndDelete(req.params.id);
-      if (!post) {
-        return res.json({ error: 'Post not found' });
-      }
-      res.json(post);
-    } catch (err) {
-      res.json({ error: `Error ${err.message}` });
-    }
-  });
+  .get(postController.getPostById)
+  .put(postController.updatePostById)
+  .delete(postController.deletePostById);
 
 module.exports = router;
