15b8c61e1d66c7ffff549063a4d9810cb7292ec3
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-05-03 16:24:31 -0600

    Define differently index, cause it didnt work

--- models/postModel.js
+++ models/postModel.js
@@ -6,6 +6,7 @@ const postSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
       required: true,
+      index: true, //created index, this worked
     },
     title: {
       type: String,
@@ -22,6 +23,7 @@ const postSchema = new mongoose.Schema(
 );
 
 ///post are often taken based on user id
-postSchema.index({ userId: 1 });
+// postSchema.index({ userId: 1 });
+//NOTE it didnt want to work
 
 module.exports = mongoose.model('Post', postSchema);
